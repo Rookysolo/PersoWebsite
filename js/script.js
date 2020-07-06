af10985e6ab8a217d12faf5d9612183ec4737402
Rookysolo <rookysolo@gmail.com>
2020-07-06 11:19:17 +0200

    responsive mode

--- js/script.js
+++ js/script.js
@@ -3,3 +3,26 @@ function myScroll(elem) {
         scrollTop: $(elem).offset().top -80
     }, 1200, 'swing');
 }
+
+
+$( document ).ready(function() {
+
+	$('.navBar-mobile-shutdownLight').hide();
+	$('.navBar-mobile ul').hide();
+	$('.navBar-mobile i.fa-times').hide();
+
+	$('.navBar-mobile i.fa-bars').click('on', function(){
+		$(this).hide();
+		$('.navBar-mobile-shutdownLight').show();
+		$('.navBar-mobile ul').show();
+		$('.navBar-mobile i.fa-times').show();
+	});
+
+	$('.navBar-mobile i.fa-times').click('on', function(){
+		$(this).hide();
+		$('.navBar-mobile-shutdownLight').hide();
+		$('.navBar-mobile ul').hide();
+		$('.navBar-mobile i.fa-bars').show();
+	});
+
+});
--- navBar.php
+++ navBar.php
@@ -1,4 +1,21 @@
+<div class="navBar-mobile-shutdownLight"></div>
+
 <div class="navBar">
+  <ul>
+    <li><a href="#" onclick="myScroll('.about_me');">Résumé</a></li>
+    <li><a href="#" onclick="myScroll('.graduations');">Diplômes</a></li>
+    <li><a href="#" onclick="myScroll('.portfolio');">Portfolio</a></li>
+    <li><a href="#" onclick="myScroll('.testimonial');">Recommandations</a></li>
+    <li><a href="#" onclick="myScroll('.contact');">Contact</a></li>
+  </ul>
+  <ul>
+    <li><a href="#">CV</a></li>
+  </ul>
+</div>
+
+<div class="navBar-mobile">
+  <i class="fas fa-bars"></i>
+  <i class="fas fa-times"></i>
   <ul>
     <li><a href="#" onclick="myScroll('.about_me');">Résumé</a></li>
     <li><a href="#" onclick="myScroll('.graduations');">Diplômes</a></li>
